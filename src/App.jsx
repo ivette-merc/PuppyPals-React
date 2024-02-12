4fda11ffed12316f9a2a6c636117f010f25a4947
ivette-merc <ivettemerc@gmail.com>
2024-02-11 19:15:30 -0500

    functionaly done

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,36 @@ import { puppyList } from "./data.js";
 function App() {
   console.log(useState);
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   console.log("puppyList: ", puppies);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <>
       <div className="App">
         <h1>Get to know our Puppies 🐶!</h1>
         {puppies.map((puppy) => {
-          return <p key={puppy.id}> {puppy.name}</p>;
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {" "}
+              {puppy.name}
+            </p>
+          );
         })}
+        {featPupId && ( //why can {featPupId} be written outside the div?
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
